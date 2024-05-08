63c33965b0a09c7a877682ab96cf5d72155badf9
dylancr27 <damolina27@hotmail.com>
2024-05-07 19:40:01 -0600

    Actualizacion Login

--- UNAprematriculaWeb/Controllers/HomeController.cs
+++ UNAprematriculaWeb/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using UNAprematriculaWeb.Models;
+using UNAprematriculaWeb.ViewModels;
 
 namespace UNAprematriculaWeb.Controllers
 {
@@ -8,9 +10,12 @@ namespace UNAprematriculaWeb.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private readonly PrematriculaUnaContext _dbContext = new PrematriculaUnaContext();
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+
         }
 
         public IActionResult Index()
@@ -27,8 +32,27 @@ namespace UNAprematriculaWeb.Controllers
         {
             return View();
         }
+
+
+
+        [HttpPost]
+        public IActionResult MainEstudiante(LoginViewModel loginViewModel)
+        {
+            Console.WriteLine(loginViewModel.usuario);
+            Console.WriteLine(loginViewModel.contrasena);
+
+            var usuarioEncontrado = _dbContext.Usuarios.Select(e => e.Email == loginViewModel.usuario && e.Contrasena == loginViewModel.contrasena).FirstOrDefault();
+
+
+
+           
+            return View();
+        }
+
+        [HttpGet]
         public IActionResult MainEstudiante()
         {
+
             return View();
         }
 
---
+++ UNAprematriculaWeb/Models/Periodo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UNAprematriculaWeb.Models;
+
+public partial class Periodo
+{
+    public int PeriodoId { get; set; }
+
+    public DateOnly FechaInicio { get; set; }
+
+    public DateOnly FechaFinal { get; set; }
+}
